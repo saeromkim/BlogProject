d37a0c6e415054cee1f956fd65fc5e9cd2d2e81e
saeromkim247 <ksr24777@gmail.com>
2019-04-26 22:09:43 +0900

    [front] 라우터와 리덕스 적용

---
+++ blog-frontend/src/store/modules/list.js
@@ -0,0 +1,16 @@
+import { createAction, handleActions } from 'redux-actions';
+
+import { Map } from 'immutable';
+import { pender } from 'redux-pender';
+
+//actions types
+
+//action creators
+
+//initial state
+const initialState = Map({});
+
+//reducer
+export default handleActions({
+
+}, initialState)
